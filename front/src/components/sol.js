7074ea6ffa3e43bca361676a1fa707f9795b8855
Santiago <s.saenz11@uniandes.edu.co>
2019-03-28 12:46:44 -0500

    funciona el put jajaja

--- front/src/components/sol.js
+++ front/src/components/sol.js
@@ -8,99 +8,126 @@ state = {
 }
 
 
-handleSubmit(id, cantidadActual , url , about, fondos) {
-    
-var canNueva = cantidadActual+100;
+handleSubmit(event) {
     
 
-console.log(id +"+");
-console.log(canNueva+"-");
-console.log(url +"+");
-console.log(about+"-");
-console.log(fondos +"+");
 
-        fetch("/solicitud/"+id, {
-            method: 'PUT',
-           
-            mode: "cors",
-            body: {"id": id,
-        "isActive": false,
-        "cantidadConseguida": 8,
-        "cantidadFaltante": canNueva,
-        "fondos": fondos,
-        "picture": url,
-        "about": about,
-        "registered": "2017-06-24T03:53:09 +05:00",
-        "latitude": -76.708446,
-        "longitude": 123.568829}
-        })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.log(error));
+    //var canNueva = cantidadActual+1000;
+var userId1 = event.target.id;
+   fetch('/solicitud/'+userId1)
+             .then(function(response) {
+                
+                 return response.json();
+             })
+             .then(myJson => {
+                
+               var x = myJson.cantidadFaltante + 1000;
 
-    } 
+               myJson.cantidadFaltante = x;
 
-    handleSubmit1(id, cantidadActual , url , about, fondos) {
-    
+               return myJson;
 
-    var canNueva = cantidadActual+500;
+               
+             }).then(myJson1 =>{
 
-   
-
-        fetch("/solicitud/"+id, {
+var y = JSON.stringify(myJson1);
+console.log("omeee"+y);
+                fetch("/solicitud/"+ userId1, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             mode: "cors",
-            body: {"id": id,
-        "isActive": false,
-        "cantidadConseguida": 8,
-        "cantidadFaltante": canNueva,
-        "fondos": fondos,
-        "picture": url,
-        "about": about,
-        "registered": "2017-06-24T03:53:09 +05:00",
-        "latitude": -76.708446,
-        "longitude": 123.568829}
+            body: y
         })
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.log(error));
 
+             });
     } 
 
-    handleSubmit2(id, cantidadActual , url , about, fondos) {
+    handleSubmit1(event) {
     
 
-    var canNueva = cantidadActual+1000;
 
-   
+    //var canNueva = cantidadActual+1000;
+var userId1 = event.target.id;
+   fetch('/solicitud/'+userId1)
+             .then(function(response) {
+                
+                 return response.json();
+             })
+             .then(myJson => {
+                
+               var x = myJson.cantidadFaltante + 500;
 
-        fetch("/solicitud/"+id, {
+               myJson.cantidadFaltante = x;
+
+               return myJson;
+
+               
+             }).then(myJson1 =>{
+
+var y = JSON.stringify(myJson1);
+console.log("omeee"+y);
+                fetch("/solicitud/"+ userId1, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             mode: "cors",
-            body: {"id": id,
-        "isActive": false,
-        "cantidadConseguida": 8,
-        "cantidadFaltante": canNueva,
-        "fondos": fondos,
-        "picture": url,
-        "about": about,
-        "registered": "2017-06-24T03:53:09 +05:00",
-        "latitude": -76.708446,
-        "longitude": 123.568829}
+            body: y
         })
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.log(error));
 
+             });
+
     } 
+
+    handleSubmit2(event) {
+    
+
+    //var canNueva = cantidadActual+1000;
+var userId1 = event.target.id;
+   fetch('/solicitud/'+userId1)
+             .then(function(response) {
+                
+                 return response.json();
+             })
+             .then(myJson => {
+                
+               var x = myJson.cantidadFaltante + 100;
+
+               myJson.cantidadFaltante = x;
+
+               return myJson;
+
+               
+             }).then(myJson1 =>{
+
+var y = JSON.stringify(myJson1);
+console.log("omeee"+y);
+                fetch("/solicitud/"+ userId1, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            mode: "cors",
+            body: y
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+
+             });
+     }
+    
 
    // <button type="button" onChange={this.handleSubmit(this.props.solicitud.id,this.props.solicitud.cantidadFaltante,this.props.solicitud.picture,this.props.solicitud.about,this.props.solicitud.fondos)} className="btn btn-outline-dark lalo">$100</button>
 //<button type="button" onChange={this.handleSubmit1(this.props.solicitud.id,this.props.solicitud.cantidadFaltante,this.props.solicitud.picture, this.props.solicitud.about,this.props.solicitud.fondos)} className="btn btn-outline-dark lalo">$500</button>
@@ -122,9 +149,11 @@ console.log(fondos +"+");
       <p className="card-text">Fondos Necesario: {this.props.solicitud.fondos}</p>
       <p className="card-text">Fondos Alcanzados : ${this.props.solicitud.cantidadFaltante}</p>
     </div>
-
+ 
     <div className="bd-example ">
-
+<button id = {this.props.solicitud.id} type="button" onClick={this.handleSubmit2} className="btn btn-outline-dark lalo">$100</button>
+ <button id = {this.props.solicitud.id} type="button" onClick={this.handleSubmit1} className="btn btn-outline-dark lalo">$500</button>
+ <button id = {this.props.solicitud.id} type="button" onClick={this.handleSubmit} className="btn btn-outline-dark lalo">$1000</button>
 
 
 
