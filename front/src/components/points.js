d4d5a85e6c0ec1b8f78b33b0db66b74f5d7855a1
Santiago <s.saenz11@uniandes.edu.co>
2019-05-18 15:36:00 -0500

    tiene componente d3

--- front/src/components/points.js
+++ front/src/components/points.js
@@ -1,12 +1,24 @@
-
-import React from 'react';
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import {FormattedMessage} from 'react-intl';
+import * as d3 from "d3";
+import { color } from 'd3-color';
+import { interpolateRgb } from 'd3-interpolate';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import LiquidFillGauge from 'react-liquid-gauge';
 
 export default class points extends React.Component {
 
-    state = {
-        "benefits_list":[]
+  state = {
+        value: 0, value1:0
+    };
+    startColor = '#6495ed'; // cornflowerblue
+    endColor = '#dc143c'; // crimson
+
+    componentWillMount(){
+
+
+        
     }
 
 componentDidMount() {
@@ -21,33 +33,182 @@ componentDidMount() {
                 return response.json();
             })
             .then(myJson => {
-                
-                this.setState({benefits_list:myJson});               
-                console.log(this.state.benefits_list);
-            });
-    }
-  render() {
-    //debugger;
-    return (
-        <div>
-            <h2><FormattedMessage id="accumulated_points"/></h2>
-            <br/>
-            <h3><FormattedMessage id="accumulated_points_value"/></h3>
-            <br/>
-            <div className="progress">
-              <div id= "barra1" className="progress-bar " role="progressbar"  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{this.state.benefits_list.pointsValue}</div>
-            </div>
-            <br/>
-            <h3><FormattedMessage id="accumulated_points_stock"/></h3>
-            <br/>
-            <div className="progress">
-              <div id= "barra2" className="progress-bar " role="progressbar"  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{this.state.benefits_list.stock}</div>
-            </div>
-            <br/>
-            <br/>
-        </div>
-    );
-  }
-}
 
+                var value2 = myJson.pointsValue;
+                var value3 = myJson.stock;
+                
+                this.setState({value:value2, value1: value3});               
+                console.log(this.state.benefits_list);
+
+            });
+
+          
+
+    }
+    render() {
+        const radius = 300;
+        const interpolate = interpolateRgb(this.startColor, this.endColor);
+        const fillColor = interpolate(this.state.value / 100);
+        const fillColor1 = interpolate(this.state.value1 / 100);
+        const gradientStops = [
+            {
+                key: '0%',
+                stopColor: color(fillColor).darker(0.5).toString(),
+                stopOpacity: 1,
+                offset: '0%'
+            },
+            {
+                key: '50%',
+                stopColor: fillColor,
+                stopOpacity: 0.75,
+                offset: '50%'
+            },
+            {
+                key: '100%',
+                stopColor: color(fillColor).brighter(0.5).toString(),
+                stopOpacity: 0.5,
+                offset: '100%'
+            }
+        ];
+         const gradientStops1 = [
+            {
+                key: '0%',
+                stopColor: color(fillColor1).darker(0.5).toString(),
+                stopOpacity: 1,
+                offset: '0%'
+            },
+            {
+                key: '50%',
+                stopColor: fillColor1,
+                stopOpacity: 0.75,
+                offset: '50%'
+            },
+            {
+                key: '100%',
+                stopColor: color(fillColor1).brighter(0.5).toString(),
+                stopOpacity: 0.5,
+                offset: '100%'
+            }
+        ];
+          
+
+        return (
+            <div>
+            <h2>Points accumulated/Stock</h2>
+<br/>
+            <div className = "row">
+            
+                <LiquidFillGauge id= "izq" className= "col-md"
+                    style={{ margin: '0 auto' }}
+                    width={radius }
+                    height={radius }
+                    value={this.state.value}
+                    percent="%"
+                    textSize={1}
+                    textOffsetX={0}
+                    textOffsetY={0}
+                    textRenderer={(props) => {
+                        const value = Math.round(props.value);
+                        const radius = Math.min(props.height / 2, props.width / 2);
+                        const textPixels = (props.textSize * radius / 2);
+                        const valueStyle = {
+                            fontSize: textPixels
+                        };
+                        const percentStyle = {
+                            fontSize: textPixels * 0.6
+                        };
+
+                        return (
+                            <tspan>
+                                <tspan className="value" style={valueStyle}>{value}</tspan>
+                                <tspan style={percentStyle}>{props.percent}</tspan>
+                            </tspan>
+                        );
+                    }}
+                    riseAnimation
+                    waveAnimation
+                    waveFrequency={2}
+                    waveAmplitude={1}
+                    gradient
+                    gradientStops={gradientStops}
+                    circleStyle={{
+                        fill: fillColor
+                    }}
+                    waveStyle={{
+                        fill: fillColor
+                    }}
+                    textStyle={{
+                        fill: color('#444').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                    waveTextStyle={{
+                        fill: color('#fff').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                    onClick={() => {
+                        this.setState({ value: Math.random() * 100 });
+                    }}
+                />
+                 <LiquidFillGauge id= "der" className= "col-md"
+                    style={{ margin: '0 auto' }}
+                    width={radius }
+                    height={radius }
+                    value={this.state.value1}
+                    percent="%"
+                    textSize={1}
+                    textOffsetX={0}
+                    textOffsetY={0}
+                    textRenderer={(props) => {
+                        const value = Math.round(props.value);
+                        const radius = Math.min(props.height / 2, props.width / 2);
+                        const textPixels = (props.textSize * radius / 2);
+                        const valueStyle = {
+                            fontSize: textPixels
+                        };
+                        const percentStyle = {
+                            fontSize: textPixels * 0.6
+                        };
+
+                        return (
+                            <tspan>
+                                <tspan className="value" style={valueStyle}>{value}</tspan>
+                                <tspan style={percentStyle}>{props.percent}</tspan>
+                            </tspan>
+                        );
+                    }}
+                    riseAnimation
+                    waveAnimation
+                    waveFrequency={2}
+                    waveAmplitude={1}
+                    gradient
+                    gradientStops1={gradientStops1}
+                    circleStyle={{
+                        fill: fillColor1
+                    }}
+                    waveStyle={{
+                        fill: fillColor1
+
+                    }}
+                    textStyle={{
+                        fill: color('#444').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                    waveTextStyle={{
+                        fill: color('#fff').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                    onClick={() => {
+                        this.setState({ value1: Math.random() * 100 });
+                    }}
+                />
+                
+                   <br/>
+                </div>
+               
+                
+            </div>
+           
+        );
+    }
+}
 
